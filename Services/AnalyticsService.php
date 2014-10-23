7a20ebfe75da33efb5209ee6e96eedc0dd1dc4e1
Matthieu Auger <matthieua@theodo.fr>
2014-10-23 17:12:51 +0200

    Fix checkstyle

--- Services/AnalyticsService.php
+++ Services/AnalyticsService.php
@@ -2,27 +2,25 @@
 
 namespace HappyR\Google\ApiBundle\Services;
 
+use \Google_Client;
+
 /**
- * Class AnalyticsService
- *
  * This is the class that communicates with analytics api
  */
 class AnalyticsService extends \Google_Service_Analytics
 {
     /**
-     * @var GoogleClient client
-     *
-     *
+     * @var GoogleClient
      */
     public $client;
 
     /**
-     * Constructor
      * @param GoogleClient $client
      */
     public function __construct(GoogleClient $client)
     {
-        $this->client=$client;
+        $this->client = $client;
+
         parent::__construct($client->getGoogleClient());
     }
 }
